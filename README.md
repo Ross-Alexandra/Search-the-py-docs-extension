79d2c0bcfc9b583f2586f1a6ec2d8efd8ee8b618
Ross Alexandra <Rossandus@hotmail.com>
2018-11-07 22:56:03 -0800

    Resolves #8. Redirecting to first valid page now words on refresh.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Search-the-py-docs-extension - Version 0.4.0
+# Search-the-py-docs-extension - Version 0.4.1
 A chrome extension to dynamically change the version of the pydocs you're viewing.
--- background.js
+++ background.js
@@ -1,44 +1,64 @@
-
 var was_forward = false;
+var was_this_request = false;
+var storageCache = {};
 
-chrome.webRequest.onBeforeRequest.addListener(function(details) {
-	var split_url = details.url.split("/");
+chrome.storage.sync.get({
+	pythonVersion: "3.0.0",
+	doRedirect: true,
+	doWarnRedirect: false
+	}, function(data) {
 
-	pyMainVer = "3" // Goal is to get this from storage somehow...
+		storageCache = data;
 
-	split_url[3] = pyMainVer + ".1.2.3.4.5";
+		chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
-	if (!was_forward) {
-		return {redirectUrl: split_url.join("/")};
-	}
-	else {
-		was_forward = false;
-	}
-},	{
-		urls: [
-			'*://docs.python.org/*'
-		],
-		types: ["main_frame"],
-	},
-	["blocking"]
-);
+			var split_url = details.url.split("/");
 
-chrome.webRequest.onCompleted.addListener(function(details) {
-	if (details.statusCode == 404) {
-		var split_url = details.url.split("/");
-		var last_version_specifier = split_url[3].lastIndexOf(".");
+			split_url[3] = storageCache["pythonVersion"];
 
-		split_url[3] = split_url[3].slice(0, last_version_specifier);
 
-		was_forward = true;
-		chrome.tabs.update({url: split_url.join("/")});
-	}
-},	{
+			if (!was_forward && details.url != split_url.join("/")) {
+				alert("Initial redirect");
+				return {redirectUrl: split_url.join("/")};
+			}
+			else {
+				was_forward = false;
+			}
+		},	{
+				urls: [
+					'*://docs.python.org/*'
+				],
+				types: ["main_frame"],
+			},
+			["blocking"]
+		);
 
-		urls: [
-		'*://docs.python.org/*'
-		],
-		types: ["main_frame", "sub_frame"]
-	}
-);
+		chrome.webRequest.onResponseStarted.addListener(function(details) {
+			if (details.statusCode != 200) {
+				var split_url = details.url.split("/");
+				var last_version_specifier = split_url[3].lastIndexOf(".");
 
+				split_url[3] = split_url[3].slice(0, last_version_specifier);
+
+				was_forward = true;
+
+				alert("updating url to: " + split_url.join("/"));
+    			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        			chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
+    			});
+			}
+		},	{
+
+				urls: [
+				'*://docs.python.org/*'
+				],
+				types: ["main_frame"],
+			}
+		);
+
+		chrome.storage.onChanged.addListener(function(changes, areaName) {
+			for (key in changes) {
+				storageCache[key] = changes[key].newValue;
+			}
+		});
+});
