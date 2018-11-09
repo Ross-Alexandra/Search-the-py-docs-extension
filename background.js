c0b9a1e91a1fe731c440e6cff06acaa02783bc53
Ross Alexandra <Rossandus@hotmail.com>
2018-11-08 22:24:13 -0800

    GH-4 - Extension now works to #4 standards when alerting exists.
    When the code alerting what steps are taken at each redirect,
    the extension fails to load past one url.

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-var was_this_request = false;
 var storageCache = {};
 
 chrome.storage.sync.get({
@@ -10,6 +9,7 @@ chrome.storage.sync.get({
 		storageCache = data;
 		var was_forward = false;
 		var most_recent_request = "";
+		var original_request = "";
 
 		chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
@@ -17,10 +17,11 @@ chrome.storage.sync.get({
 
 			split_url[3] = storageCache["pythonVersion"];
 
-			if (!was_forward && details.url != most_recent_request) {
-				//alert("Initial redirect -- from: " + details.url + " -- most recent request: " + most_recent_request);
-				most_recent_request = split_url.join("/");
-				return {redirectUrl: most_recent_request};
+			if (!was_forward && details.url.split("/")[3] != most_recent_request) {
+				alert("Initial redirect -- from: " + details.url + " -- most recent request: " + most_recent_request);
+				most_recent_request = split_url[3]
+				original_request = details.url.split("/")[3];
+				return {redirectUrl: split_url.join("/")};
 			}
 		},	{
 				urls: [
@@ -38,14 +39,22 @@ chrome.storage.sync.get({
 
 				split_url[3] = split_url[3].slice(0, last_version_specifier);
 
-				most_recent_request = split_url.join("/");
-				if (most_recent_request[most_recent_request.length - 1] != '/') {
-					most_recent_request += "/";
+				if (split_url[3] == "") {
+					alert("Ran out of options -- reverting to: " + original_request);
+					most_recent_request = original_request;
+					split_url[3] = original_request;
+					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        				chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
+    				});
+
+					return;
 				}
 
-				//alert("updating url to: " + split_url.join("/") + " from " + details.url);
+				most_recent_request = split_url[3];
+
+				alert("updating url to: " + split_url.join("/") + " from " + details.url);
     			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        			chrome.tabs.update(tabs[0].id, {url: most_recent_request});
+        			chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
     			});
 			}
 			else {
