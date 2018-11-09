8771157d86c05f9f52e56b6c635452d5d4382adb
Ross Alexandra <Rossandus@hotmail.com>
2018-11-08 23:09:21 -0800

    Resolved #4.  Also removes checkboxes from options page.
    The removal of the checkboxesis because their actions need
    to be better thought out before continuing.

--- background.js
+++ background.js
@@ -7,18 +7,24 @@ chrome.storage.sync.get({
 	}, function(data) {
 
 		storageCache = data;
-		var was_forward = false;
 		var most_recent_request = "";
 		var original_request = "";
 
+		// On the inital request, load the user's prefered version of
+		// python.
 		chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
 			var split_url = details.url.split("/");
 
+			// The 4th position in the url is the python version.
 			split_url[3] = storageCache["pythonVersion"];
 
-			if (!was_forward && details.url.split("/")[3] != most_recent_request) {
-				alert("Initial redirect -- from: " + details.url + " -- most recent request: " + most_recent_request);
+			// If this request wasn't the most recent request
+			if (details.url.split("/")[3] != most_recent_request) {
+
+				// Set this to the most recent request, and
+				// redirect to the python docs version
+				// stored in the user's preferences.
 				most_recent_request = split_url[3]
 				original_request = details.url.split("/")[3];
 				return {redirectUrl: split_url.join("/")};
@@ -27,38 +33,47 @@ chrome.storage.sync.get({
 				urls: [
 					'*://docs.python.org/*'
 				],
-				types: ["main_frame"],
+				types: ["main_frame", "sub_frame"],
 			},
 			["blocking"]
 		);
 
+		// Catches redirects that 404. This is used to find an appropriate
+		// version of the py docs for the user that is closes to their
+		// specific version.
 		chrome.webRequest.onResponseStarted.addListener(function(details) {
+
 			if (details.statusCode == 404) {
 				var split_url = details.url.split("/");
 				var last_version_specifier = split_url[3].lastIndexOf(".");
 
+				// Because this page 404'ed, we need to go the the next sub
+				// version. Ex, if 3.7.1 failed, try 3.7
 				split_url[3] = split_url[3].slice(0, last_version_specifier);
 
-				if (split_url[3] == "") {
-					alert("Ran out of options -- reverting to: " + original_request);
-					most_recent_request = original_request;
-					split_url[3] = original_request;
-					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        				chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
-    				});
+				// Set a timeout for 10ms as we must wait a short period of time
+				// otherwise this request will get overshadowed (for unknown reasons.)
+				setTimeout(function() {
 
-					return;
-				}
+					// If none of the user's sub-versions worked, redirect to the
+					// originally queried page. Ex, Query 3.0, redirect to 1.1.1,
+					// that 404's, go to 1.1, that fails, go to 1, that fails, give
+					// up and return to 3.0
+					if (split_url[3] == "") {
+						most_recent_request = original_request;
+						split_url[3] = original_request;
+						chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        					chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
+    					});
+					}
+					else {
+						most_recent_request = split_url[3];
 
-				most_recent_request = split_url[3];
-
-				alert("updating url to: " + split_url.join("/") + " from " + details.url);
-    			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        			chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
-    			});
-			}
-			else {
-				redirected = false;
+    					chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        					chrome.tabs.update(tabs[0].id, {url: split_url.join("/")});
+    					});
+					}
+				}, 10);
 			}
 		},	{
 
@@ -66,9 +81,10 @@ chrome.storage.sync.get({
 				'*://docs.python.org/*'
 				],
 				types: ["main_frame"],
-			}
+			},
 		);
 
+		// If storage is updated, update the cache.
 		chrome.storage.onChanged.addListener(function(changes, areaName) {
 			for (key in changes) {
 				storageCache[key] = changes[key].newValue;
