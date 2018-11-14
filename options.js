0a6373b2be6232a5b3594ff98d5049029f33d323
Ross Alexandra <Rossandus@hotmail.com>
2018-11-13 19:58:04 -0800

    Resolved #15 - Removes check options from the option page.

--- options.js
+++ options.js
@@ -4,14 +4,10 @@ function save_options() {
 	python_version.push(document.getElementById("subPyVer").value);
 	python_version.push(document.getElementById("buildPyVer").value);
 
-	var do_redirect = document.getElementById("redirect").checked;
-	var do_warn_redirect = document.getElementById("warn_redirect").checked;
-
 	chrome.storage.sync.set({
-		pythonVersion: python_version.join("."),
-		doRedirect: do_redirect,
-		doWarnRedirect: do_warn_redirect
+		pythonVersion: python_version.join(".")
 	}, function() {
+
 		//update status to let users know options were saved.
 		var status = document.getElementById("status");
 		status.textContent = "Options Saved.";
@@ -25,21 +21,15 @@ function save_options() {
 //Restores select box and checkbox state using the preferences.
 function restore_options() {
 
-	// Default is 3.0.0 and do_redirect = false
+	// Default is 3.0.0
 	chrome.storage.sync.get({
-		pythonVersion: "3.0.0",
-		doRedirect: true,
-		doWarnRedirect: false
+		pythonVersion: "3.0.0"
 	}, function(items) {
 		var python_versions = items.pythonVersion.split(".");
 
 		document.getElementById("mainPyVer").value = python_versions[0];
 		document.getElementById("subPyVer").value = python_versions[1];
 		document.getElementById("buildPyVer").value = python_versions[2];
-
-		document.getElementById("redirect").checked = items.doRedirect;
-
-		document.getElementById("warn_redirect").checked = items.doWarnRedirect;
 	});
 }
 
