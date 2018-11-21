1b42ed7b247bbbff65507b1470adcd12ab63b9e6
ross3834 <rossandus@hotmail.com>
2018-11-21 14:14:45 -0800

    Resolves #11 - Added the functionality to the popup.

---
+++ popup.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('ok_button').onclick = searchDocs;
+
+	document.getElementById('searchQuery').onkeydown = isKeyEnter;
+	document.onkeydown = isKeyEnter;
+});
+
+function isKeyEnter(event) {
+	if (event.keycode == 13) {
+		searchDocs();
+	}
+}
+
+function searchDocs() {
+	var query = document.getElementById("searchQuery").value;
+
+	chrome.storage.sync.get({
+        	pythonVersion: "3.0.0",
+        	warnRedirect: false,
+        	}, function(data) {
+			chrome.tabs.create({url: "https://docs.python.org/" + data.pythonVersion  + "/search.html?q=" + query  + "&check_keywords=yes&area=default"});
+		}
+	);
+}
